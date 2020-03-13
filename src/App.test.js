43c7e26d15975e3b1be75ae6d5b38e3128bd4885
fluxlog <fluxlog@hotmail.com>
2020-03-12 18:03:49 -0700

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import * as rtl from '@testing-library/react';
 import App from "./App";
+
+
+
+afterEach(rtl.cleanup);
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("renders submit form header", () => {
+
+  const { getByText } = render(<App />);
+
+  const header = getByText(/submit/i);
+
+  expect(header).toBeInTheDocument();
+
+}) 
+
